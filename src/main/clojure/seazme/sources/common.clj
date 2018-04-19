b4b23954e3d08b94c9f5964c4ed21d1a1f5fa7aa
AndyL <andy.innersource@gmail.com>
2018-04-19 09:56:17 -0700

    common: parallel

--- src/main/clojure/seazme/sources/common.clj
+++ src/main/clojure/seazme/sources/common.clj
@@ -5,3 +5,7 @@
 
 
 (defn file-exists [f])
+(defn to-edn [f d] (spit f (pr-str d)))
+(defn from-edn [f] (read-string (slurp f)))
+
+(defn pmapr[r f coll] (->> coll (partition-all r) (mapcat #(->> % (pmap f) doall))))
